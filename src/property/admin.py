aee2c2496eb591d41626394266b28b2e43443545
jsm <sang93423@gmail.com>
2019-02-25 18:57:28 +0900

    admin에서 category출력

--- src/property/admin.py
+++ src/property/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Property
+from .models import Property, Category
 
 admin.site.register(Property)
+admin.site.register(Category)
