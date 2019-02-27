98cdaffb279d90c143b06bf334a39f0c02b81d0f
jsm <sang93423@gmail.com>
2019-02-27 17:12:52 +0900

    메인(home)페이지 설정 완료, our service부분 아직 데이터화 시키지 않음

--- src/home/views.py
+++ src/home/views.py
@@ -1,13 +1,16 @@
 from django.shortcuts import render
 from property.models import Property, Category
+from agents.models import Agent
 
 
 def home(request):
     category_list = Category.objects.all()
     property_list = Property.objects.all()
+    agent_list = Agent.objects.all()
     template = 'home/home.html'
     context = {
-        'category_list': category_list,
-        'property_list': property_list,
+        'category_list_home': category_list,
+        'property_list_home': property_list,
+        'agent_list_home': agent_list,
     }
     return render(request, template, context)
