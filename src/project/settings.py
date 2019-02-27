747f55195c128adc44dec1a55da02c0112c75939
jsm <sang93423@gmail.com>
2019-02-27 15:24:41 +0900

    EMAIL_BACKEND 추가

--- src/project/settings.py
+++ src/project/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'property',
     'agents',
     'about',
+    'contact',
 ]
 
 MIDDLEWARE = [
@@ -131,3 +132,10 @@ STATICFILES_DIRS = [
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, '.media')
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+DEFAULT_FROM_EMAIL = 'test@gmail.com'
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
+EMAIL_USE_TLS = False
+EMAIL_PORT = 1025
