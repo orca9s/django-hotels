c862f407f308d1137481633a96557eaeabee7b26
jsm <sang93423@gmail.com>
2019-02-28 19:36:14 +0900

    property_search_url연결

--- src/property/urls.py
+++ src/property/urls.py
@@ -6,5 +6,6 @@ app_name = 'property'
 
 urlpatterns = [
     path('', views.property_list, name='property_list'),
+    path('search/', views.property_search_list, name='property_search_list'),
     path('<int:id>', views.property_detail, name='property_detail'),
 ]
